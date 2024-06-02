0f50af179661122ec1bc4035d4491d340891abaf
Aditya Baxi <147598016+Adityabaxi001@users.noreply.github.com>
2024-06-03 00:01:27 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,1 +1,20 @@
+#numpy arrays
+import numoy as np
+a= [1,2,3,4]
+b= np.array(a)
+print(b)
 
+#creating 2D array
+import numoy as np
+x= np.array([[1,2,3,4],[1,2,3,4]])
+print(x)
+
+#creating 3D array
+import numpy as np
+u=np.array([[[1,2,3,4],[1,2,3,4],[1,2,3,4]]])
+print(u)
+
+#creating nd array
+import numpy as np
+h= np.array([1,2,3,4],ndmin=10)
+print(h)
